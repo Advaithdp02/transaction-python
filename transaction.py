0abfaf43bf641a46de5581bc65029af1d3f1114a
Advaithdp02 <advaithdp@gmail.com>
2024-08-01 22:37:46 +0530

    importing modules

--- transaction.py
+++ transaction.py
@@ -2,4 +2,15 @@ import pandas as pd
 import csv
 from datetime import datetime
 
-class CSV()
+class CSV():
+    csv_file="finance_data.csv"
+    @classmethod
+    def intialize_csv(cls):
+        try:
+            pd.read_csv(cls.csv_file)
+            
+        except FileNotFoundError:
+            df=pd.DataFrame(columns=["data","amount","catgory","description"])
+            df.to_csv(cls.csv_file,index=False)
+CSV.intialize_csv()
+
