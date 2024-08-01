3d564b8e47c43873c0725164a9a2361f9a74d7bd
Advaithdp02 <advaithdp@gmail.com>
2024-08-01 22:53:12 +0530

    addd entry method

--- transaction.py
+++ transaction.py
@@ -4,13 +4,28 @@ from datetime import datetime
 
 class CSV():
     csv_file="finance_data.csv"
+    columns=["date","amount","category","description"]
     @classmethod
     def intialize_csv(cls):
         try:
             pd.read_csv(cls.csv_file)
-            
+
         except FileNotFoundError:
-            df=pd.DataFrame(columns=["data","amount","catgory","description"])
+            df=pd.DataFrame(columns=cls.columns)
             df.to_csv(cls.csv_file,index=False)
+    
+    @classmethod
+    def add_entry(cls,date,amount,category,description):
+        new_entry={
+            "date":date,
+            "amount":amount,
+            "category":category,
+            "description":description
+        }
+        with open(cls.csv_file,'a',newline="") as csvfile:
+            writer = csv.DictWriter(csvfile,fieldnames=cls.columns)
+            writer.writerow(new_entry)
+        print("ENTRY ADDED SUCCESSFULLY") 
 CSV.intialize_csv()
+CSV.add_entry('20-07-2024',200.00,'Income','Salary')
 
